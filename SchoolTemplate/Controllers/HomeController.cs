d43fbfd1c83ad7ada95c97f29d1fe1a78877bca5
Alex Ander <Nooitnooitnietzeggen@outlook.com>
2020-09-11 14:29:28 +0200

    T doet t

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using SchoolTemplate.Database;
@@ -8,59 +9,134 @@ using SchoolTemplate.Models;
 
 namespace SchoolTemplate.Controllers
 {
-  public class HomeController : Controller
-  {
-    // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=110082;Uid=110082;Pwd=agNARDOR;";
-
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-      List<Product> products = new List<Product>();
-      // uncomment deze regel om producten uit je database toe te voegen
-      products = GetProducts();
+        // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
+        string connectionString = "Server=172.16.160.21;Port=3306;Database=110082;Uid=110082;Pwd=agNARDOR;";
 
-      return View(products);
-    }
-
-    private List<Product> GetProducts()
-    {
-      List<Product> products = new List<Product>();
-
-      using (MySqlConnection conn = new MySqlConnection(connectionString))
-      {
-        conn.Open();
-        MySqlCommand cmd = new MySqlCommand("select * from product", conn);
-
-        using (var reader = cmd.ExecuteReader())
+        public IActionResult Index()
         {
-          while (reader.Read())
-          {
-            Product p = new Product
-            {
-              Id = Convert.ToInt32(reader["Id"]),
-              Naam = reader["Naam"].ToString(),
-              Calorieen = float.Parse(reader["calorieen"].ToString()),
-              Formaat = reader["Formaat"].ToString(),
-              Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
-              Prijs = Decimal.Parse(reader["Prijs"].ToString())
-            };
-            products.Add(p);
-          }
+            List<Festival> festival = new List<Festival>();
+            // uncomment deze regel om producten uit je database toe te voegen
+            festival = GetFestivals();
+
+            return View(festival);
         }
-      }
 
-      return products;
+        [Route("privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("show-all")]
+        public IActionResult ShowAll()
+        {
+            return View(GetFestivals());
+        }
+
+        [Route("festival/{id}")]
+        public IActionResult Festival(int id)
+        {
+            var model = GetFestival(id);
+            if (model == null)
+            {
+                return View("404");
+            }
+
+            return View(model);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private Festival GetFestival(int id)
+        {
+            List<Festival> festivals = new List<Festival>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand($"select * from festival where id = {id}", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Festival p = new Festival
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Naam = reader["Naam"].ToString(),
+
+                        };
+                        festivals.Add(p);
+                    }
+                }
+            }
+            return festivals.FirstOrDefault();
+        }
+
+        private List<Festival> GetFestivals()
+        {
+            List<Festival> festivals = new List<Festival>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from festival limit 10", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Festival p = new Festival
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Naam = reader["Naam"].ToString(),
+
+                        };
+                        festivals.Add(p);
+                    }
+                }
+            }
+
+            return festivals;
+        }
+        private List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from product limit 10", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Product p = new Product
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Naam = reader["Naam"].ToString(),
+                            Calorieen = float.Parse(reader["calorieen"].ToString()),
+                            Formaat = reader["Formaat"].ToString(),
+                            Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
+                            Prijs = Decimal.Parse(reader["Prijs"].ToString())
+                        };
+                        products.Add(p);
+                    }
+                }
+            }
+
+            return products;
+        }
+
     }
 
-    public IActionResult Privacy()
-    {
-      return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-  }
+   
 }
